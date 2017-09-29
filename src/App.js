df0f485eb50b08d9ba2adc5600813dad48fb0c28
fellyph <fellyph.cintra@gmail.com>
2017-09-29 01:26:15 +0100

    create method getOffers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import Offer from './components/offer/offer'
+import axios from 'axios'
+
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      offers: [],
+    }
+  }
+
+  getOffer () {
+    return (e) => {
+      axios.get('https://drive.google.com/file/d/0B-cd6ovbirHacFN0UlpDRzY0dWs/view')
+        .then((result) => {
+          this.setState({offers: result})
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Findhotel</h1>
         </header>
-        <Offer />
       </div>
     );
   }
